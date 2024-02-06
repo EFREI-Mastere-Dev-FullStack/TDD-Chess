315ddccca5fd98f464a3d7f301144c302b739444
Idris TAKERBOUST <idrista@icloud.com>
2024-02-06 17:04:00 +0100

    test: Prawn: move forward

--- spec/pawn.spec.rb
+++ spec/pawn.spec.rb
@@ -1,9 +1,13 @@
 require_relative '../piece/pawn'
 
 describe Pawn do
-  context '#move' do
-    it 'move in diagonal' do
+  context '#valid_move?' do
+    chessboard = ChessBoard.new
 
+    it 'should return true if pawn moves forward' do
+      pawn = Pawn.new(current_coordinate: 'A2')
+      result  = valid_move?('A3')
+      expect(result).to eq(true)
     end
   end
 end
