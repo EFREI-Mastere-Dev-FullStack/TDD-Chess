ec81a0b46067f9fe10416e169e2736d632900bea
Idris TAKERBOUST <idrista@icloud.com>
2024-02-06 16:42:25 +0100

    refactoring: get_position

--- chess_board.rb
+++ chess_board.rb
@@ -9,17 +9,29 @@ class ChessBoard
   end
 
   def get_position_by_coordinates(coordinates)
-    raise RangeError if coordinates.size > 2 || coordinates.size < 2
+    raise RangeError unless valid_coordinates?(coordinates)
+
+    row = 8 - coordinates[1].to_i
+    column = coordinates[0].upcase.ord - 'A'.ord
+
+    [row, column]
+  end
+
+  def valid_coordinates?(coordinates)
+    return false unless coordinates.size == 2
 
     letter = coordinates[0]
     number = coordinates[1]
 
-    raise RangeError unless letter.match?(/[[:alpha:]]/)
-    raise RangeError if letter.upcase.ord > 'H'.ord
-
-    column = letter.upcase.ord - 'A'.ord
-    row = 8 - number.to_i
-
-    [row, column]
+    valid_letter?(letter) && valid_number?(number)
   end
+
+  def valid_letter?(letter)
+    letter.match?(/[[:alpha:]]/) && letter.upcase.ord <= 'H'.ord
+  end
+
+  def valid_number?(number)
+    number.match?(/[[:digit:]]/) && number.to_i <= CHESS_DIM
+  end
+
 end
