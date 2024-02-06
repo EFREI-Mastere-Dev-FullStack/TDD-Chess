f8b10a528154f9901e49574ad4735fce0a9b0b00
Idris TAKERBOUST <idrista@icloud.com>
2024-02-06 15:43:08 +0100

    test: get specific position by coordinates

--- spec/chess_board.spec.rb
+++ spec/chess_board.spec.rb
@@ -1,11 +1,18 @@
 require_relative '../chess_board'
 
 describe ChessBoard do
-  context "#initialize" do
-    it "creates an 8x8 chessboard by default" do
+  context '#initialize' do
+    it 'creates an 8x8 chessboard by default' do
       chessboard = ChessBoard.new
       expect(chessboard.board).to be_an Array
       expect(chessboard.board.all? { |row| row.is_a?(Array) }) .to eq(true)
+    end
+  end
+
+  context '#get_position_by_name' do
+    it 'return the position in the corner' do
+      result = get_position_by_coordinates('A1')
+      expect(result).to eq([0,7])
     end
   end
 end
